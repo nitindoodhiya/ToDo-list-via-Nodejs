a8a5311b3bc35353d766e0c1f21e21b7c1661e5e
Nick Nitin <nitindoodhiya@gmail.com>
2018-12-14 18:36:22 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var todoController = require('./controller/todoController');
+
+var app =express();
+//set View
+app.set('view engine','ejs');
+
+
+//static files
+app.use(express.static('./public'));
+
+//controllers
+todoController(app);
+//listen 30000
+app.listen(3000);
---
+++ controller/todoController.js
@@ -0,0 +1,24 @@
+var data=[{item:'React'},{item:'Node JS'},{item: 'postgreSQL'}];
+
+var bodyParser= require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
+module.exports = function (app) {
+
+app.get('/todo',function (req,res) {
+  res.render('todo',{todos:data});
+});
+
+app.post('/todo', urlencodedParser ,function (req,res) {
+ data.push(req.body);
+ console.log(data);
+ res.json(data);
+});
+
+app.delete('/todo/:item',function (req,res) {
+  data = data.filter(function (todo) {
+    return todo.item.replace(/ /g, '-') !==req.params.item;
+  });
+  res.json(data);
+});
+}
